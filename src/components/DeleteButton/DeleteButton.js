669758615cc19af2879cb4ad1283dc4ce4906a50
Vincent Chang <vincentchang402@gmail.com>
2020-10-11 16:12:53 +0800

    Rename remove action and dispatch by clicking delete button

--- src/components/DeleteButton/DeleteButton.js
+++ src/components/DeleteButton/DeleteButton.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import idx from 'idx';
+import { removeById } from '../../reducer/slices/users';
 import styles from './DeleteButton.style';
 
-const DeleteButton = () => (
-  <button className="delete-button" type="button">
-    Delete
-    <style jsx>{styles}</style>
-  </button>
-);
+const DeleteButton = ({ item }) => {
+  const dispatch = useDispatch();
+  return (
+    <button
+      className="delete-button"
+      type="button"
+      onClick={() => dispatch(removeById({ id: idx(item, (_) => _.id) }))}
+    >
+      Delete
+      <style jsx>{styles}</style>
+    </button>
+  );
+};
 
 export default DeleteButton;

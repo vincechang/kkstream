5dd127e1dcbbcf852226e72a1d03a4e5fcaab2a4
Vincent Chang <vincentchang402@gmail.com>
2020-10-11 11:44:25 +0800

    Configure root reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+const client = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  responseType: 'json',
+});
+
+const reducer = combineReducers({});
 
 export default configureStore({
-  reducer: {},
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(axiosMiddleware(client)),
 });

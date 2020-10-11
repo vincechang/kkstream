91dd1dbf007c8d520faeda2afbc9db37200b3c7d
Vincent Chang <vincentchang402@gmail.com>
2020-10-11 13:55:03 +0800

    Add layout of delete button

--- src/components/UserTable/UserTable.js
+++ src/components/UserTable/UserTable.js
@@ -3,11 +3,31 @@ import { connect } from 'react-redux';
 import { selectUsers, fetchUsers } from '../../reducer/slices/users';
 import styles from './UserTable.style';
 
+const DeleteButton = () => (
+  <button className="delete-button" type="button">
+    Delete
+    <style jsx>{`
+      .delete-button {
+        margin-bottom: 7px;
+
+        &:hover {
+          color: red;
+          border: 1px solid red;
+        }
+
+        &:focus {
+          outline: none;
+        }
+      }
+    `}</style>
+  </button>
+);
+
 const columnSpec = [
   { header: 'ID', column: 'id' },
   { header: 'Name', column: 'name' },
   { header: 'E-mail', column: 'email' },
-  { header: 'Delete', column: 'delete' },
+  { header: 'Delete', column: 'delete', util: DeleteButton },
 ];
 
 class UserTable extends Component {
@@ -15,10 +35,10 @@ class UserTable extends Component {
     this.props.fetchUsers();
   }
 
-  renderColumn(item, { column }) {
+  renderColumn(item, { column, util }) {
     return (
       <td>
-        {item[column]}
+        {util ? util() : item[column]}
         <style jsx>{styles}</style>
       </td>
     );

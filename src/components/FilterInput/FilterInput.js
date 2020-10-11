c6868cc310747f693ae28d3418e82e4d85b24e75
Vincent Chang <vincentchang402@gmail.com>
2020-10-12 01:51:40 +0800

    Add set filter action, filter selector to reducer and use in component

--- src/components/FilterInput/FilterInput.js
+++ src/components/FilterInput/FilterInput.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import idx from 'idx';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectFilter, setFilter } from '../../reducer/slices/users';
 import styles from './FilterInput.style';
 
 const FilterInput = () => {
+  const filter = useSelector(selectFilter);
+  const dispatch = useDispatch();
   return (
     <Fragment>
       <input
         className="filter-input"
         type="text"
         placeholder="Input to filter user name"
+        value={filter}
+        onChange={(e) => {
+          dispatch(setFilter(idx(e, (_) => e.target.value)));
+        }}
       ></input>
       <style jsx>{styles}</style>
     </Fragment>

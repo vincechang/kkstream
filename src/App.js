2e91c1ad89a604db049ba898f52500507b7e04c3
Vincent Chang <vincentchang402@gmail.com>
2020-10-11 19:47:26 +0800

    Dispatch open modal action when the button of add user was clicked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import UserTable from './components/UserTable/UserTable';
 import AddUserModal from './components/AddUserModal/AddUserModal';
+import { openAddUserModal } from './reducer/slices/ui';
 import './App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
   const { isOpenAddUserModal } = useSelector((state) => state.ui);
   return (
     <div className="App">
       <header className="App-header">
-        <button className="add-user-button">Add User</button>
+        <button
+          className="add-user-button"
+          onClick={() => dispatch(openAddUserModal())}
+        >
+          Add User
+        </button>
         <style jsx>{`
           .add-user-button {
             margin-left: auto;

e5c578f94de108178a816faa5d50bb427592e1e0
Vincent Chang <vincentchang402@gmail.com>
2020-10-11 18:16:04 +0800

    Refactor app to function component and connect ui reducer to it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import UserTable from './components/UserTable/UserTable';
 import AddUserModal from './components/AddUserModal/AddUserModal';
 import './App.css';
 
-function App() {
+const App = () => {
+  const { isOpenAddUserModal } = useSelector((state) => state.ui);
   return (
     <div className="App">
       <header className="App-header">
         <UserTable />
-        <AddUserModal isOpen />
+        <AddUserModal isOpen={isOpenAddUserModal} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
